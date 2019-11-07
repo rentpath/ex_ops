1a2e3c3a4da74e2c2fd9f95d30b88aa0b1cb9daa
Trevor Brown <trevorbrown@rentpath.com>
2019-11-07 07:51:05 -0500

    Update BuildDetails code to handle both plain file paths and priv file paths

--- lib/build_details.ex
+++ lib/build_details.ex
@@ -17,15 +17,30 @@ defmodule ExOps.BuildDetails do
   defp build_info_regex, do: Application.get_env(:ex_ops, :build_info_regex)
   defp deploy_info_regex, do: Application.get_env(:ex_ops, :deploy_info_regex)
 
+  defp resolve_path(info_file) do
+    file_path = Map.get(info_file, :path)
+    case Map.get(info_file, :type) do
+      :priv_dir ->
+        application = Map.get(info_file, :application)
+        :erlang.iolist_to_binary([:code.priv_dir(application), file_path])
+      :path  ->
+        file_path
+    end
+  end
+
   defp build_info(build_info_file, deploy_info_file) do
-    case read_and_parse(build_info_file, build_info_regex()) do
+    case build_info_file
+    |> resolve_path()
+    |> read_and_parse(build_info_regex()) do
       {:ok, details} -> build_details(details, deploy_info_file)
       _ -> build_details(nil, nil)
     end
   end
 
-  defp deploy_info_date(file_name) do
-    case read_and_parse(file_name, deploy_info_regex()) do
+  defp deploy_info_date(deploy_file_info) do
+    case deploy_file_info
+    |> resolve_path()
+    |> read_and_parse(deploy_info_regex()) do
       {:ok, [deploy_date]} -> deploy_date
       _ -> ""
     end
--- test/build_details_test.exs
+++ test/build_details_test.exs
@@ -6,10 +6,10 @@ defmodule ExOps.BuildDetailsTest do
 
     test "default response when files do not exist" do
       info_files = %{
-        build_info_file: "foo",
-        deploy_info_file: "bar",
-        previous_build_info_file: "baz",
-        previous_deploy_info_file: nil,
+        build_info_file: %{type: :path, path: "foo"},
+        deploy_info_file: %{type: :path, path: "bar"},
+        previous_build_info_file: %{type: :path, path: "baz"},
+        previous_deploy_info_file: nil
       }
 
       build_defaults = %BuildDetails{}
@@ -18,37 +18,50 @@ defmodule ExOps.BuildDetailsTest do
       assert BuildDetails.full(info_files) == detail_info
     end
 
-    test "response when files exist" do
-      info_files = %{
-        build_info_file: "test/support/fixtures/BUILD-INFO-TEST",
-        deploy_info_file: "test/support/fixtures/DEPLOY-INFO-TEST",
-        previous_build_info_file: "test/support/fixtures/PREVIOUS-BUILD-INFO-TEST",
-        previous_deploy_info_file: "test/support/fixtures/PREVIOUS-DEPLOY-INFO-TEST"
+    def valid_info_files do
+      %{
+        build_info_file: %{
+          type: :path,
+          path: "test/support/fixtures/BUILD-INFO-TEST"
+        },
+        deploy_info_file: %{
+          type: :path,
+          path: "test/support/fixtures/DEPLOY-INFO-TEST"
+        },
+        previous_build_info_file: %{
+          type: :path,
+          path: "test/support/fixtures/PREVIOUS-BUILD-INFO-TEST"
+        },
+        previous_deploy_info_file: %{
+          type: :path,
+          path: "test/support/fixtures/PREVIOUS-DEPLOY-INFO-TEST"
+        }
       }
+    end
+
+    test "response when files exist" do
       detail_info = Map.merge(build_details(), previous_details())
 
-      assert BuildDetails.full(info_files) == detail_info
+      assert BuildDetails.full(valid_info_files()) == detail_info
     end
 
     test "response when some file format does not match" do
-      info_files = %{
-        build_info_file: "test/support/fixtures/BUILD-INFO-BAD-FORMAT",
-        deploy_info_file: "test/support/fixtures/DEPLOY-INFO-TEST",
-        previous_build_info_file: "test/support/fixtures/PREVIOUS-BUILD-INFO-TEST",
-        previous_deploy_info_file: "test/support/fixtures/PREVIOUS-DEPLOY-INFO-TEST"
-      }
+      info_files = Map.merge(valid_info_files(), %{
+        build_info_file: %{
+          type: :path,
+          path: "test/support/fixtures/BUILD-INFO-BAD-FORMAT"
+        },
+      })
       detail_info = Map.merge(%BuildDetails{}, previous_details())
 
       assert BuildDetails.full(info_files) == detail_info
     end
 
     test "response when previus build info does not exist" do
-      info_files = %{
-        build_info_file: "test/support/fixtures/BUILD-INFO-TEST",
-        deploy_info_file: "test/support/fixtures/DEPLOY-INFO-TEST",
-        previous_build_info_file: "foo",
+      info_files = Map.merge(valid_info_files(), %{
+        previous_build_info_file: %{type: :path, path: "foo"},
         previous_deploy_info_file: nil
-      }
+      })
 
       detail_info = Map.merge(%{previous: %BuildDetails{}}, build_details())
 
@@ -56,13 +69,36 @@ defmodule ExOps.BuildDetailsTest do
     end
 
     test "response when previus build info exist but no current details" do
-      info_files = %{
-        build_info_file: "foo",
-        deploy_info_file: nil,
-        previous_build_info_file: "test/support/fixtures/PREVIOUS-BUILD-INFO-TEST",
-        previous_deploy_info_file: "test/support/fixtures/PREVIOUS-DEPLOY-INFO-TEST"
-      }
+      info_files = Map.merge(valid_info_files(), %{
+        build_info_file: %{type: :path, path: "foo"},
+        deploy_info_file: nil
+      })
+
       detail_info = Map.merge(%BuildDetails{}, previous_details())
+
+      assert BuildDetails.full(info_files) == detail_info
+    end
+
+    test "response when current and previous build info does not exist" do
+      info_files = %{
+        build_info_file: %{type: :path, path: "foo"},
+        deploy_info_file: nil,
+        previous_build_info_file: %{type: :path, path: "bar"},
+        previous_deploy_info_file: nil
+      }
+      detail_info = Map.merge(%BuildDetails{}, %{previous: %BuildDetails{}})
+
+      assert BuildDetails.full(info_files) == detail_info
+    end
+
+    test "response when config specifies files in the priv dir" do
+      info_files = %{
+        build_info_file: %{type: :priv_dir, application: :ex_ops, path: "foo"},
+        deploy_info_file: nil,
+        previous_build_info_file: %{type: :priv_dir, application: :ex_ops, path: "bar"},
+        previous_deploy_info_file: nil
+      }
+      detail_info = Map.merge(%BuildDetails{}, %{previous: %BuildDetails{}})
 
       assert BuildDetails.full(info_files) == detail_info
     end
